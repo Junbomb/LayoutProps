26d14fbca4d45a2ce409b3ff5b9c003ca4639ffa
Junbomb <dkfkgks2030@gmail.com>
2021-10-13 12:23:47 +0900

    Read More

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Button, ScrollView, View , StyleSheet} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <View Style={[styles.container, styles.playingSpace]}>
+            
+            </View>
+            <ScrollView style={[styles.container]}>
+                <View style={[styles.controlSpace]}>
+                    <View style={[styles.buttonView]}>
+                        <Button title="Learn More"/>
+                    </View>
+                    <View style={[styles.buttonView]}>
+                        <Button title="1"/>
+                    </View>
+                    <View style={[styles.buttonView]}>
+                        <Button title="2"/>
+                    </View>
+                    <View style={[styles.buttonView]}>
+                        <Button title="3"/>
+                    </View>
+                    <View style={[styles.buttonView]}>
+                        <Button title="4"/>
+                    </View>
+                    <View style={[styles.buttonView]}>
+                        <Button title="5"/>
+                    </View>
+                    <View style={[styles.buttonView]}>
+                        <Button title="6"/>
+                    </View>
+                    <View style={[styles.buttonView]}>
+                        <Button title="7"/>
+                    </View>
+
+
+</View>
+            
+            </ScrollView>
+        </>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container:{
+        height: '50%',
+    },
+    playingSpace: {
+        backgroundColor: 'white',
+        borderColor: 'blue',
+        borderWidth: 3,
+    },
+    controlSpace: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        backgroundColor: '#F5F5F5',
+    },
+    ButtonView: {
+            width:'50%',
+            padding: 10,
+    }
+
+})
+
+export default App;
