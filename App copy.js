a5efe25dbf2fa123561812725a550d27c77c7fcf
Junbomb <dkfkgks2030@gmail.com>
2021-10-29 01:31:55 +0900

    과제2

--- App copy.js
+++ App copy.js
@@ -1,30 +1,13 @@
 import React from "react";
-import {Image, Text, Button, ScrollView, View, StyleSheet } from "react-native";
-import Constants from 'expo-constants';
-
-/*
-TEST
-*/
+import {Button, ScrollView, View , StyleSheet} from "react-native";
+import Constants from 'react-native'
 
 const App = () => {
     return (
         <>
-            <View style={{paddingTop: Constants.statusBarHeight}}/>
-            <View style={[styles.container, styles.playingSpace]}>
-                <Text> Hello </Text>
-                <Text> Hello </Text>
-                <Text> Hello </Text>
-                <Text> 대중음악 딴따라라 폄하, 대표적인 차별 문제
-                    미국 밥딜런은 노벨상도..우리는 왜 차별하나?
-                    대중문화도 문화예술, 시대에 맞게 포함시켜야
-                    면제가 아니라 국위선양을 위한 대체복무방식
-                    전문연구요원 병역특례처럼 공정기준 마련 可
-                    정치권은 공론장 여는 역할, 국민여론에 따라.. 
-
-                </Text>
-                <Image style={styles.tinyLogo}
-                    source={{uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}/>
+            <View Style={{paddingTop: Constants.statusBarHeight}}/>
+            <View Style={[styles.container, styles.playingSpace]}>
+            
             </View>
             <ScrollView style={[styles.container]}>
                 <View style={[styles.controlSpace]}>
@@ -76,10 +59,6 @@ const styles = StyleSheet.create({
     ButtonView: {
             width:'50%',
             padding: 10,
-    },
-    tinyLogo: {
-        width: 100,
-        height: 100,
     }
 
 })
